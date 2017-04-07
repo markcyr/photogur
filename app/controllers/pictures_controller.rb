e7092057aae94ca53391efa88a11a074cf27c386
markcyr <markcyr@msn.com>
2017-04-07 16:09:45 -0400

    Adding feature to edit pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -19,12 +19,24 @@ class PicturesController < ApplicationController
       end
     end
 
-
-
-
     def show
       @picture = Picture.find(params[:id])
     end
+
+    def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+
+    if @picture.update_attributes(picture_params)
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      render :edit
+    end
+  end
+
 
     private
     def picture_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'pictures#index'
-  
+
   get 'pictures' => 'pictures#index' # this is the list of pictures
 
   post 'pictures' => 'pictures#create' # this is to call create
@@ -10,5 +10,8 @@ Rails.application.routes.draw do
   get 'pictures/new' => 'pictures#new' # this is to create a new picture
 
   get 'pictures/:id' => 'pictures#show', as: 'picture'
+
+  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+  patch 'pictures/:id' => "pictures#update"
 
 end
